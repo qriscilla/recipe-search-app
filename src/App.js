87adb56b00d844d4d1c77f3e2be4ea061e6c0562
qriscilla <pri.whang@gmail.com>
2020-01-23 08:22:02 -0500

    Left off at 28:38

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 
 import Form from './components/Form';
 
+const APP_ID = '4327c824';
+const APP_KEY = '57824ae7544369a3c67c79a495bf48d0';
+
 class App extends Component {
-  getRecipe = (e) => {
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log(recipeName);
+    const api_call = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`); // The API Hamza used, Food2Fork is no longer available, so I am using Edamam API here.
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.hits });
+    console.log(this.state.recipes);
   }
+
   render() {
     return (
       <div className='App'>
@@ -16,6 +28,9 @@ class App extends Component {
           <h1 className='App-title'>Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
+        { this.state.recipes.map((recipe) => {
+          return <p key={recipe.label}>{ recipe.label }</p>
+        }) }
       </div>
     );
   }
