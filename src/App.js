d16af5f07825cd385e99aea39fdcbf36191a2837
qriscilla <pri.whang@gmail.com>
2020-01-23 20:28:49 -0500

    Finished up to right before adding React router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 
 import Form from './components/Form';
+import Recipes from './components/Recipes';
 
 const APP_ID = '4327c824';
 const APP_KEY = '57824ae7544369a3c67c79a495bf48d0';
@@ -14,7 +15,7 @@ class App extends Component {
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`); // The API Hamza used, Food2Fork is no longer available, so I am using Edamam API here.
+    const api_call = await fetch(`https://api.edamam.com/search?q=${recipeName}&app_id=${APP_ID}&app_key=${APP_KEY}`); // The API Hamza used, Food2Fork is no longer available, so I am using Edamam API here.
 
     const data = await api_call.json();
     this.setState({ recipes: data.hits });
@@ -28,9 +29,7 @@ class App extends Component {
           <h1 className='App-title'>Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe} />
-        { this.state.recipes.map((recipe) => {
-          return <p key={recipe.label}>{ recipe.label }</p>
-        }) }
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 // a stateless functional component
 const Form = props => (
-    <form onSubmit={props.getRecipe}>
-        <input type='text' name='recipeName' />
-        <button>Search</button>
+    <form onSubmit={props.getRecipe} style={{marginBottom:'2rem'}}>
+        <input className='form__input' type='text' name='recipeName' />
+        <button className='form__button'>Search</button>
     </form>
 );
 
