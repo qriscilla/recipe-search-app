0456c3e62ebbbd5745ba8af05d37ef3554efef07
qriscilla <pri.whang@gmail.com>
2020-02-03 18:39:07 -0500

    Updates

---
+++ precache-manifest.b99d320d18ff947530e2954921d1e97f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a710e14063097fa1ca20c76693df19d",
+    "url": "/recipe-search-app/index.html"
+  },
+  {
+    "revision": "92739616a222f723c87c",
+    "url": "/recipe-search-app/static/css/2.1087be86.chunk.css"
+  },
+  {
+    "revision": "ef1563fea5f7c647218a",
+    "url": "/recipe-search-app/static/css/main.a2e6583f.chunk.css"
+  },
+  {
+    "revision": "92739616a222f723c87c",
+    "url": "/recipe-search-app/static/js/2.71185cf2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe-search-app/static/js/2.71185cf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef1563fea5f7c647218a",
+    "url": "/recipe-search-app/static/js/main.76ad239a.chunk.js"
+  },
+  {
+    "revision": "48ee4a53fdf063abde39",
+    "url": "/recipe-search-app/static/js/runtime-main.0a14e74c.js"
+  }
+]);
